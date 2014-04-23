4f9d346e95d5c727874b3cdd17f9ab1b890ce25a
Manuel Quiñones <manuel.por.aca@gmail.com>
2014-04-23 18:56:38 -0300

    Move graph to a separate module

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ from gi.repository import GdkPixbuf
 from gi.repository import MyPaint
 
 from xsheet import XSheet
+from canvasgraph import CanvasGraph
 from canvaswidget import CanvasWidget
 from xsheetwidget import XSheetWidget
 from metronome import Metronome
@@ -18,19 +19,6 @@ from giutils import set_base_value, get_base_value, set_base_color
 
 _settings = get_settings()
 
-def print_connections(node):
-    def print_node(node, i=0, pad=''):
-        print("  " * i + ' ' + pad + ' ' + node.get_operation())
-        # FIMXE use gegl_operation_list_properties if that is in the
-        # introspection bindings
-        for input_pad in ['input', 'aux']:
-            connected_node = node.get_producer(input_pad, None)
-            if connected_node is not None:
-                print_node(connected_node, i+1, input_pad)
-
-    print_node(node)
-    print("")
-
 
 class Application(GObject.GObject):
     def __init__(self):
@@ -38,19 +26,14 @@ class Application(GObject.GObject):
 
         self._set_default_settings()
 
+        self._xsheet = XSheet(24 * 60)
+
+        self._canvas_graph = CanvasGraph(self._xsheet)
+        self._metronome = Metronome(self._xsheet)
+
         self._canvas_widget = None
         self._xsheet_widget = None
 
-        self._graph = None
-        self._nodes = {}
-
-        self._xsheet = XSheet(24 * 60)
-        self._xsheet.connect('frame-changed', self._xsheet_changed_cb)
-        self._xsheet.connect('layer-changed', self._xsheet_changed_cb)
-
-        self._metronome = Metronome(self._xsheet)
-
-        self._create_graph()
         self._setup_icons()
         self._init_ui()
 
@@ -87,98 +70,6 @@ class Application(GObject.GObject):
             factory.add(name, iconset)
             factory.add_default()
 
-    def _create_graph(self):
-        self._graph = Gegl.Node()
-
-        main_over = self._graph.create_child("gegl:over")
-        self._nodes['main_over'] = main_over
-
-        layer_overs = []
-        for l in range(self._xsheet.layers_length):
-            over = self._graph.create_child("gegl:over")
-            layer_overs.append(over)
-
-        self._nodes['layer_overs'] = layer_overs
-
-        layer_overs[0].connect_to("output", main_over, "input")
-
-        for over, next_over in zip(layer_overs, layer_overs[1:]):
-            next_over.connect_to("output", over, "input")
-
-        background_node = self._graph.create_child("gegl:rectangle")
-        background_node.set_property('color', Gegl.Color.new("#fff"))
-        background_node.connect_to("output", layer_overs[-1], "input")
-        self._nodes['background'] = background_node
-
-        layer_nodes = []
-        for l in range(self._xsheet.layers_length):
-            nodes = {}
-            current_cel_over = self._graph.create_child("gegl:over")
-            current_cel_over.connect_to("output", layer_overs[l], "aux")
-            nodes['current_cel_over'] = current_cel_over
-
-            onionskin_overs = []
-            onionskin_opacities = []
-            for i in range(_settings['onionskin']['length']):
-                over = self._graph.create_child("gegl:over")
-                onionskin_overs.append(over)
-
-                opacity = self._graph.create_child("gegl:opacity")
-                falloff = _settings['onionskin']['falloff']
-                opacity.set_property('value', 1 - falloff)
-                onionskin_opacities.append(opacity)
-
-                over.connect_to("output", opacity, "input")
-
-                for over, next_opacity in zip(onionskin_overs,
-                                              onionskin_opacities[1:]):
-                    next_opacity.connect_to("output", over, "aux")
-
-                onionskin_opacities[0].connect_to("output", current_cel_over,
-                                                  "aux")
-
-            nodes['onionskin'] = {}
-            nodes['onionskin']['overs'] = onionskin_overs
-            nodes['onionskin']['opacities'] = onionskin_opacities
-            layer_nodes.append(nodes)
-
-        self._nodes['layer_nodes'] = layer_nodes
-
-        self._xsheet.set_graph(self._graph)
-        self._update_graph()
-
-    def _update_graph(self):
-        get_cel = None
-        if _settings['onionskin']['by_cels']:
-            get_cel = self._xsheet.get_cel_relative_by_cels
-        else:
-            get_cel = self._xsheet.get_cel_relative
-
-        for layer_idx in range(self._xsheet.layers_length):
-            layer_nodes = self._nodes['layer_nodes'][layer_idx]
-            cur_cel = self._xsheet.get_cel(layer_idx=layer_idx)
-
-            if cur_cel is not None:
-                cur_cel.surface_node.connect_to(
-                    "output", layer_nodes['current_cel_over'], "input")
-            else:
-                layer_nodes['current_cel_over'].disconnect("input")
-
-            if not _settings['onionskin']['on']:
-                continue
-
-            layer_diff = layer_idx - self._xsheet.layer_idx
-            for i in range(_settings['onionskin']['length']):
-                prev_cel = get_cel(-(i+1), layer_diff=layer_diff)
-                over = layer_nodes['onionskin']['overs'][i]
-
-                if prev_cel is not None:
-                    prev_cel.surface_node.connect_to("output", over, "input")
-                else:
-                    over.disconnect("input")
-
-        # debug
-        # print_connections(self._nodes['main_over'])
 
     def _init_ui(self):
         window = Gtk.Window()
@@ -252,11 +143,12 @@ class Application(GObject.GObject):
         next_layer_button.show()
 
         self._canvas_widget = CanvasWidget(
-            self._xsheet, root_node=self._nodes['main_over'])
+            self._xsheet, root_node=self._canvas_graph.root_node)
         top_box.attach(self._canvas_widget, 0, 1, 1, 1)
         self._canvas_widget.show()
 
-        self._canvas_widget.view.connect("size-allocate", self._size_allocate_cb)
+        self._canvas_widget.view.connect("size-allocate",
+                                         self._size_allocate_cb)
 
         self._xsheet_widget = XSheetWidget(self._xsheet)
         top_box.attach(self._xsheet_widget, 1, 1, 1, 1)
@@ -266,12 +158,9 @@ class Application(GObject.GObject):
         Gtk.main_quit()
 
     def _size_allocate_cb(self, widget, allocation):
-        background_node = self._nodes['background']
+        background_node = self._canvas_graph.nodes['background']
         background_node.set_property("width", allocation.width)
         background_node.set_property("height", allocation.height)
-
-    def _xsheet_changed_cb(self, xsheet):
-        self._update_graph()
 
     def _toggle_play_stop(self):
         if self._xsheet.is_playing:
@@ -282,23 +171,8 @@ class Application(GObject.GObject):
     def _toggle_play_cb(self, widget):
         self._toggle_play_stop()
 
-    def _toggle_onionskin(self):
-        _settings['onionskin']['on'] = not _settings['onionskin']['on']
-
-        for layer_idx in range(self._xsheet.layers_length):
-            layer_nodes = self._nodes['layer_nodes'][layer_idx]
-            onionskin_opacities = layer_nodes['onionskin']['opacities']
-            current_cel_over = layer_nodes['current_cel_over']
-            if _settings['onionskin']['on']:
-                onionskin_opacities[0].connect_to("output", current_cel_over,
-                                                  "aux")
-            else:
-                current_cel_over.disconnect("aux")
-
-        self._update_graph()
-
     def _toggle_onionskin_cb(self, widget):
-        self._toggle_onionskin()
+        self._canvas_graph.toggle_onionskin()
 
     def _toggle_eraser(self):
         _settings['eraser']['on'] = not _settings['eraser']['on']
@@ -358,7 +232,7 @@ class Application(GObject.GObject):
         if event.keyval == Gdk.KEY_p:
             self._toggle_play_stop()
         elif event.keyval == Gdk.KEY_o:
-            self._toggle_onionskin()
+            self._canvas_graph.toggle_onionskin()
         elif event.keyval == Gdk.KEY_e:
             self._toggle_eraser()
         elif event.keyval == Gdk.KEY_BackSpace:
